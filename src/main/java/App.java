08e019971d260d87ce8ff130e72fcb9b8ba61686
Vladimir Vanifatov <vanfat2000@gmail.com>
2023-09-04 18:20:29 +0400

    fix

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,12 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class App {
+    static Logger logger = Logger.getLogger(App.class.getName());
+
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
@@ -9,8 +14,11 @@ public class App {
         HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
         Cat bean3 = (Cat) applicationContext.getBean("cat");
         Cat bean4 = (Cat) applicationContext.getBean("cat");
-        System.out.println(bean1 == bean2);
-        System.out.println(bean3 == bean4);
+        logger.log(Level.INFO, "The result of the expression \"bean1 == bean2\" is " +
+                (bean1 == bean2));
+        logger.log(Level.INFO, "The result of the expression \"bean3 == bean4\" is " +
+                (bean3 == bean4));
+
 
     }
 }
